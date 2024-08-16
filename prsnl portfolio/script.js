9ed00656270e9a42c3849c8debe38e3b18d5cf97
Sujal Dattani <86008393+sujal-dattani@users.noreply.github.com>
2024-08-16 22:29:47 +0530

    Add files via upload

---
+++ prsnl portfolio/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.text', {
+    strings: ['Frontend Developer', 'UI/UX Designer','Web Developer'],
+    typeSpeed: 90,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+
+  });
